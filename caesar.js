dabce24a056fa3e15d92b8ed7dae63028704c25a
Alexander Thieler <alexander.thieler@gmail.com>
2018-12-08 19:30:09 -0500

    Add Caesar Cipher & LinkedIn link & Scratch Project link

---
+++ caesar.js
@@ -0,0 +1,54 @@
+window.onload = function() {
+
+	var plainText = document.getElementById('plaintext');
+	var cipherText = document.getElementById('ciphertext');
+
+	var plainTextShift = document.getElementById('plantext-shift');
+
+	var encodeButton = document.getElementById('encode-button');
+	var decodeButton = document.getElementById('decode-button');
+
+	encodeButton.onclick = function() {
+
+		var text = plainText.value.toLowerCase();
+		var shift = Number(plainTextShift.value);
+		var cipher = '';
+
+		for (var i = 0; i < text.length; i++) {
+			var c = text.charCodeAt(i);
+			if (c >= 97 && c <= 122) {
+				c -= 97;
+				c = (c + shift) % 26;
+				c += 97;
+			}
+			c = String.fromCharCode(c);
+			cipher += c;
+		}
+
+		cipherText.value = cipher;
+
+	}
+
+	decodeButton.onclick = function() {
+
+		var cipher = cipherText.value.toLowerCase();
+		var shift = -Number(plainTextShift.value);
+		var text = '';
+
+		for (var i = 0; i < cipher.length; i++) {
+			var c = cipher.charCodeAt(i);
+			if (c >= 97 && c <= 122) {
+				c -= 97;
+				c = (c + shift + 26) % 26;
+				c += 97;
+			}
+			c = String.fromCharCode(c);
+			text += c;
+		}
+
+		plainText.value = text;
+
+	}
+
+}
+
